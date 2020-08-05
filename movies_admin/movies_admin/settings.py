fcbecde4cd56bfaacee27981065856799484e907
pablomigliassoms <pablo.migliasso@dnamicworld.com>
2020-08-04 22:10:45 -0300

    Fix settings

--- movies_admin/movies_admin/settings.py
+++ movies_admin/movies_admin/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'movies_admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
